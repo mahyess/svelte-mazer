d56d2fdbba235fee718e421585cfb25f992d7674
Mahesh <mahyess@users.noreply.github.com>
2022-02-07 18:58:14 +0545

    add: alert dismiss transition

--- src/lib/components/alerts/Alert.svelte
+++ src/lib/components/alerts/Alert.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { fly } from 'svelte/transition'
 	import type { Size, Variant } from './types'
 
 	export let variant: Variant = 'primary'
@@ -8,29 +9,31 @@
 	export let size: Size = 'md'
 	export let dismissible: Boolean = false
 
-	let isShow: Boolean = true
+	let isVisible: Boolean = true
 </script>
 
-<div
-	class:d-none={!isShow}
-	class:rounded-pill={rounded}
-	class:alert-dismissible={dismissible}
-	class="
+{#if isVisible}
+	<div
+		transition:fly
+		class:rounded-pill={rounded}
+		class:alert-dismissible={dismissible}
+		class="
 		{$$props.class}
 		alert
 		alert-{size}
 		alert-{outline ? 'outline-' : light ? 'light-' : ''}{variant}
 	"
-	role="alert"
->
-	<slot />
-	{#if dismissible}
-		<button
-			on:click={() => (isShow = !isShow)}
-			type="button"
-			class="btn-close"
-			data-bs-dismiss="alert"
-			aria-label="Close"
-		/>
-	{/if}
-</div>
+		role="alert"
+	>
+		<slot />
+		{#if dismissible}
+			<button
+				on:click={() => (isVisible = !isVisible)}
+				type="button"
+				class="btn-close"
+				data-bs-dismiss="alert"
+				aria-label="Close"
+			/>
+		{/if}
+	</div>
+{/if}
