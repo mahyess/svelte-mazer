f770f9f7237a1315edd22f44a33d90e5aaa529b8
Mahesh <mahyess@users.noreply.github.com>
2022-02-07 19:28:29 +0545

    add: badge component added

---
+++ src/lib/components/badges/Badge.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	export let variant:
+		| 'primary'
+		| 'secondary'
+		| 'success'
+		| 'danger'
+		| 'warning'
+		| 'info'
+		| 'light'
+		| 'dark'
+		| 'transparent'
+		| '' = ''
+	export let light: boolean = false
+	export let color: string = ''
+	$: if (!color && !variant) {
+		variant = 'primary'
+	}
+	// export let size: 'sm' | 'lg' | 'xl' | 'xs' | 'sm-only' | 'lg'
+</script>
+
+<span class="badge bg-{light ? 'light-' : ''}{variant}" style="--theme-color: {color}">
+	<slot />
+</span>
+
+<style>
+	span {
+		background-color: var(--theme-color);
+	}
+</style>
